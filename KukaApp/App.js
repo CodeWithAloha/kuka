d98ab3b88405ada6a846651f4801fa3038329939
John Zhou <john.km.zhou@gmail.com>
2020-08-05 19:22:21 -1000

    Add navigation.

--- KukaApp/App.js
+++ KukaApp/App.js
@@ -1,5 +1,5 @@
 /**
- * Sample React Native App
+ * Kuka React Native App
  * https://github.com/facebook/react-native
  *
  * Generated with the UI Kitten template
@@ -9,7 +9,7 @@
  *
  * @format
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import {
@@ -22,33 +22,24 @@ import {
 } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+import { AgendaScreen } from './screens/AgendaScreen';
 
-/**
- * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
- * https://akveo.github.io/eva-icons
- */
-const HeartIcon = (props) => (
-  <Icon {...props} name='heart'/>
-);
+const { Navigator, Screen } = createStackNavigator();
 
 export default () => (
   <>
-    <IconRegistry icons={EvaIconsPack}/>
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <Layout style={styles.container}>
-        <Text style={styles.text} category='h1'>
-          Welcome to UI Kitten 😻
-        </Text>
-        <Text style={styles.text} category='s1'>
-          Start with editing App.js to configure your App
-        </Text>
-        <Text style={styles.text} appearance='hint'>
-          For example, try changing theme to Dark by using eva.dark
-        </Text>
-        <Button style={styles.likeButton} accessoryLeft={HeartIcon}>
-          LIKE
-        </Button>
-      </Layout>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={eva.dark}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Navigator>
+            <Screen name="Agenda" component={AgendaScreen}></Screen>
+          </Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </ApplicationProvider>
   </>
 );
---
+++ KukaApp/README.md
@@ -0,0 +1,11 @@
+# Kukā App
+
+## Setup
+
+Follow instructions here to set up your development environment: https://reactnative.dev/docs/environment-setup
+
+`yarn install` to download dependencies.
+
+`npx pod-install ios` to install and link native libraries.
+
+`yarn ios` to run the app in the iOS simulator.
