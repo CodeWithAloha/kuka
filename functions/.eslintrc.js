86a6dc16695d2f7691d40ac56e911023b5cb6875
davidrideout <david@snowpoint.org>
2020-10-02 16:34:11 -0700

    Update gitignore

---
+++ functions/.eslintrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "tsconfig.json",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "eslint-plugin-import",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "@typescript-eslint/adjacent-overload-signatures": "error",
+        "@typescript-eslint/no-empty-function": "error",
+        "@typescript-eslint/no-empty-interface": "warn",
+        "@typescript-eslint/no-floating-promises": "error",
+        "@typescript-eslint/no-namespace": "error",
+        "@typescript-eslint/no-unnecessary-type-assertion": "error",
+        "@typescript-eslint/prefer-for-of": "warn",
+        "@typescript-eslint/triple-slash-reference": "error",
+        "@typescript-eslint/unified-signatures": "warn",
+        "comma-dangle": "warn",
+        "constructor-super": "error",
+        "eqeqeq": [
+            "warn",
+            "always"
+        ],
+
+        // https://github.com/typescript-eslint/typescript-eslint/issues/2315
+        "import/no-duplicates": "off",
+
+        "import/no-deprecated": "warn",
+        "import/no-extraneous-dependencies": "error",
+        "import/no-unassigned-import": "warn",
+        "no-cond-assign": "error",
+        "no-duplicate-case": "error",
+        "no-duplicate-imports": "error",
+        "no-empty": [
+            "error",
+            {
+                "allowEmptyCatch": true
+            }
+        ],
+        "no-invalid-this": "error",
+        "no-new-wrappers": "error",
+        "no-redeclare": "error",
+        "no-sequences": "error",
+        "no-shadow": [
+            "error",
+            {
+                "hoist": "all"
+            }
+        ],
+        "no-throw-literal": "error",
+        "no-unsafe-finally": "error",
+        "no-unused-labels": "error",
+        "no-var": "warn",
+        "no-void": "error",
+        "prefer-const": "warn"
+    }
+};
