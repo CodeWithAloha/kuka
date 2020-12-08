43a329f18d2a63b8583271e2fb8c9798fa5efc82
John Zhou <john.km.zhou@gmail.com>
2020-12-07 19:53:15 -1000

    Add bottom tabbar.

---
+++ KukaApp/navigation/AuthNavigator.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Icon,
+  BottomNavigation,
+  BottomNavigationTab,
+} from '@ui-kitten/components';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AgendaNavigator } from './AgendaNavigator';
+
+const BottomTabBar = ({ navigation, state }) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={index => navigation.navigate(state.routeNames[index])}
+  >
+    <BottomNavigationTab
+      title="AGENDA"
+      icon={props => <Icon {...props} name="home-outline" />}
+    />
+    <BottomNavigationTab
+      title="TESTIMONIES"
+      icon={props => <Icon {...props} name="message-circle-outline" />}
+    />
+    <BottomNavigationTab
+      title="MY ACCOUNT"
+      icon={props => <Icon {...props} name="person-outline" />}
+    />
+    <BottomNavigationTab
+      title="HELP"
+      icon={props => <Icon {...props} name="question-mark-circle-outline" />}
+    />
+  </BottomNavigation>
+);
+
+export const AuthNavigator = () => {
+  const { Navigator, Screen } = createBottomTabNavigator();
+  return (
+    <Navigator tabBar={props => <BottomTabBar {...props} />}>
+      <Screen name="Agenda" component={AgendaNavigator} />
+    </Navigator>
+  );
+};
