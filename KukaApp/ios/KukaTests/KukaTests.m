1019721f24f933d8981b2aa593b9485f02b421bc
John Zhou <john.km.zhou@gmail.com>
2021-02-08 20:04:06 -1000

    Rename app and bundle id.

--- KukaApp/ios/KukaTests/KukaTests.m
+++ KukaApp/ios/KukaTests/KukaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface KukaAppTests : XCTestCase
+@interface KukaTests : XCTestCase
 
 @end
 
-@implementation KukaAppTests
+@implementation KukaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
