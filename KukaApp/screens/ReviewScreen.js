2fa78ef99a343e61fd34a78492e52f5f2b932aee
John Zhou <john.km.zhou@gmail.com>
2021-11-15 19:19:02 -1000

    Validate form. Check for single testimony restriction. Handle errors.

--- KukaApp/screens/ReviewScreen.js
+++ KukaApp/screens/ReviewScreen.js
@@ -7,33 +7,108 @@ import storage from '@react-native-firebase/storage';
 import 'react-native-get-random-values';
 import { v1 as uuidv1 } from 'uuid';
 import { useDocumentDataOnce } from 'react-firebase-hooks/firestore';
-import { Input, Button, Spinner } from '@ui-kitten/components';
+import {
+  Input,
+  Button,
+  Spinner,
+  Text,
+  Icon,
+  Modal,
+  Card,
+  useTheme,
+} from '@ui-kitten/components';
 import { TopNav } from '../components/TopNav';
 
 export const ReviewScreen = ({ navigation, route }) => {
+  const theme = useTheme();
   const user = auth().currentUser;
   const profileRef = firestore().collection('users').doc(user.uid);
   const [profile, isProfileLoading, hasProfileError] =
     useDocumentDataOnce(profileRef);
+  const [position, setPosition] = useState();
+  const [positionCaption, setPositionCaption] = useState(false);
   const [name, setName] = useState(user.displayName);
+  const [nameCaption, setNameCaption] = useState(false);
   const [email, setEmail] = useState(user.email);
+  const [emailCaption, setEmailCaption] = useState(false);
   const [zipCode, setZipCode] = useState(profile?.zipCode);
+  const [zipCodeCaption, setZipCodeCaption] = useState(false);
   const [lobbyGroup, setLobbyGroup] = useState(profile?.lobbyGroup);
   const [uploading, setUploading] = useState(false);
+  const [uploadError, setUploadError] = useState();
 
   const submit = async () => {
     try {
-      setUploading(true);
-      const ref = storage().ref(
-        '/agenda-items/' + route.params.agendaId + '/' + uuidv1() + '.mp4'
-      );
-      await ref.putFile(route.params.uri);
+      if (!position) {
+        setPositionCaption(true);
+      }
+
+      if (!name) {
+        setNameCaption(true);
+      }
+
+      if (!email) {
+        setEmailCaption(true);
+      }
+
+      if (!zipCode) {
+        setZipCodeCaption(true);
+      }
+
+      if (position && name && zipCode && email) {
+        setUploading(true);
+        setPositionCaption(false);
+        setNameCaption(false);
+        setEmailCaption(false);
+        setZipCodeCaption(false);
+
+        const agendaRef = firestore()
+          .collection('agendaItems')
+          .doc(route.params.agendaId);
+
+        const testimony = await agendaRef
+          .collection('testimonies')
+          .where('userId', '==', user.uid)
+          .get();
+
+        if (testimony.empty) {
+          const ref = storage().ref(
+            '/agenda-items/' + route.params.agendaId + '/' + uuidv1() + '.mp4'
+          );
+          const snapshot = await ref.putFile(route.params.uri);
+
+          agendaRef.collection('testimonies').add({
+            userId: user.uid,
+            position,
+            name,
+            email,
+            zipCode,
+            position,
+            lobbyGroup,
+            createdAt: firestore.FieldValue.serverTimestamp(),
+            fullPath: snapshot.metadata.fullPath,
+          });
+        } else {
+          setUploadError('Only one testimony allowed per agenda.');
+        }
+      }
     } catch (error) {
       console.error(error);
+      setUploadError(error);
     } finally {
       setUploading(false);
     }
   };
+
+  const handlePosition = pos => {
+    setPosition(pos);
+  };
+
+  const renderCaption = message => (
+    <View>
+      <Text status="danger">{message}</Text>
+    </View>
+  );
 
   return (
     <View style={{ flex: 1 }}>
@@ -45,12 +120,66 @@ export const ReviewScreen = ({ navigation, route }) => {
           controls
           paused
         />
+        <View
+          style={{
+            paddingTop: 8,
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+          }}
+        >
+          <Button
+            onPress={() => handlePosition('Approve')}
+            status={position === 'Approve' ? 'primary' : 'basic'}
+            appearance="outline"
+          >
+            <View style={styles.positionView}>
+              <Icon
+                style={styles.icon}
+                fill="#8F9BB3"
+                name="arrow-circle-up-outline"
+              />
+              <Text>Approve</Text>
+            </View>
+          </Button>
+          <Button
+            onPress={() => handlePosition('Disapprove')}
+            status={position === 'Disapprove' ? 'primary' : 'basic'}
+            appearance="outline"
+          >
+            <View style={styles.positionView}>
+              <Icon
+                style={styles.icon}
+                fill="#8F9BB3"
+                name="arrow-circle-down-outline"
+              />
+              <Text>Disapprove</Text>
+            </View>
+          </Button>
+          <Button
+            onPress={() => handlePosition('Comment')}
+            status={position === 'Comment' ? 'primary' : 'basic'}
+            appearance="outline"
+          >
+            <View style={styles.positionView}>
+              <Icon
+                style={styles.icon}
+                fill="#8F9BB3"
+                name="message-circle-outline"
+              />
+              <Text>Comment</Text>
+            </View>
+          </Button>
+        </View>
+        {positionCaption ? (
+          <Text status="danger">Missing position.</Text>
+        ) : null}
         <View style={{ padding: 16 }}>
           <Input
             value={name}
             label="NAME"
             placeholder="John Doe"
             onChangeText={nextValue => setName(nextValue)}
+            caption={nameCaption ? renderCaption('Missing name.') : null}
           />
           <Input
             value={email}
@@ -58,6 +187,7 @@ export const ReviewScreen = ({ navigation, route }) => {
             placeholder="user@email.com"
             onChangeText={nextValue => setEmail(nextValue)}
             style={styles.formField}
+            caption={emailCaption ? renderCaption('Missing email.') : null}
           />
           <Input
             value={zipCode}
@@ -65,6 +195,7 @@ export const ReviewScreen = ({ navigation, route }) => {
             placeholder="96818"
             onChangeText={nextValue => setZipCode(nextValue)}
             style={styles.formField}
+            caption={zipCodeCaption ? renderCaption('Missing zip code.') : null}
           />
           <Input
             value={lobbyGroup}
@@ -76,21 +207,37 @@ export const ReviewScreen = ({ navigation, route }) => {
           <Button
             onPress={submit}
             style={styles.formField}
-            accessoryLeft={props =>
-              uploading ? (
-                <View style={props.style}>
-                  <Spinner size="small" />
-                </View>
-              ) : null
-            }
             disabled={uploading}
           >
             SUBMIT TESTIMONY
           </Button>
         </View>
       </ScrollView>
+      <Modal
+        visible={uploading || uploadError}
+        backdropStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+      >
+        {uploading && <Spinner />}
+        {uploadError && (
+          <Card>
+            <View
+              style={{
+                alignItems: 'center',
+              }}
+            >
+              <Text status="danger" style={{ marginBottom: 16 }}>
+                {uploadError}
+              </Text>
+              <Button onPress={() => setUploadError('')}>Close</Button>
+            </View>
+          </Card>
+        )}
+      </Modal>
     </View>
   );
 };
 
-const styles = StyleSheet.create({ formField: { marginTop: 10 } });
+const styles = StyleSheet.create({
+  formField: { marginTop: 10 },
+  icon: { width: 48, height: 64 },
+});
